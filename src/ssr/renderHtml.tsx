d543ab02bebdbd9dec7dfea6fbf0b661adb73b62
Alexander Fedorenko <alfedmix@gmail.com>
2024-03-23 15:13:59 +0100

    Add i18n for localization.

--- src/ssr/renderHtml.tsx
+++ src/ssr/renderHtml.tsx
@@ -5,18 +5,41 @@ import { renderRoutes } from "react-router-layout";
 import { HelmetProvider } from "react-helmet-async";
 import serialize from "serialize-javascript";
 import { RootContextProvider } from "react-reducer-ssr";
+import { I18nextProvider } from "react-i18next";
+
+function getInitiali18nState(req: any) {
+  const initialLanguage = req.i18n?.languages?.[0];
+  let initialI18nStore: any;
+  const usedNamespaces = req.i18n?.reportNamespaces?.getUsedNamespaces();
+
+  if (usedNamespaces) {
+    initialI18nStore = {};
+    req.i18n?.languages?.forEach((language: string) => {
+      initialI18nStore[language] = {};
+
+      usedNamespaces.forEach((namespace: string) => {
+        initialI18nStore[language][namespace] =
+          req.i18n.services.resourceStore.data[language][namespace];
+      });
+    });
+  }
+  return { initialLanguage, initialI18nStore };
+}
 
 export const renderHtml = (reducers: any, htmlTemplate: string, routes: any, req: any, store: any) => {
+  const { initialLanguage, initialI18nStore } = getInitiali18nState(req);
   const helmetContext: any = {};
-  const contentJsx = <Router location={req.url}>
-    <HelmetProvider context={helmetContext}>
-      <RootContextProvider reducer={reducers} initialState={store?.root}>
-        <Routes>
-          {renderRoutes(routes)}
-        </Routes>
-      </RootContextProvider>
-    </HelmetProvider>
-  </Router>;
+  const contentJsx = <I18nextProvider i18n={req.i18n}>
+    <Router location={req.url}>
+      <HelmetProvider context={helmetContext}>
+        <RootContextProvider reducer={reducers} initialState={store?.root}>
+          <Routes>
+            {renderRoutes(routes)}
+          </Routes>
+        </RootContextProvider>
+      </HelmetProvider>
+    </Router>
+  </I18nextProvider>;
   const content = renderToString(contentJsx);
 
   const isProd = process.env.NODE_ENV === 'production';
@@ -28,7 +51,9 @@ export const renderHtml = (reducers: any, htmlTemplate: string, routes: any, req
 
   const html = htmlTemplate.replace('<div id="app"></div>', `<div id="app">${content}</div>
       <script>
-        window.INITIAL_STATE = ${serialize(store.root)}
+        window.INITIAL_STATE = ${serialize(store.root)};
+        window.INITIAL_LANGUAGE = '${initialLanguage}';
+        window.INITIAL_I18N_STORE = ${serialize(initialI18nStore)};
       </script>
       ${serviceWorkerScript}`)
       .replace('<!-- META -->', `${helmetTitle}
